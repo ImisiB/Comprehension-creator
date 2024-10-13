dd25410e4284150ccba71162161b69299a3f945b
Imisi Bailey <imisibailey@gmail.com>
2024-10-13 14:37:10 +0100

    Created a comprehension-creator

--- Comprehension-creator.py
+++ Comprehension-creator.py
@@ -1,151 +1,112 @@
-# print("WELCOME TO A COMPREHENSION CREATOR")
-# print("I AM GOING TO ASK SOME QUESTIONS AND YOU WILL HAVE TO ANSWER THEM.")
-# print()
+print("WELCOME TO A COMPREHENSION CREATOR")
+print("I AM GOING TO ASK SOME QUESTIONS AND YOU WILL HAVE TO ANSWER THEM.")
+print()
 
-# def main():
-#     user_name = input("What is your name: ")
-#     user_info = input("Where do you come from? : ")
-#     user_favorite_color = input("What is your favorite color? : ")
-#     user_favorite_food = input("What is your favorite food? : ")
-#     user_work = input("What do you do for a living? :")
-#     user_works = input("Are you a web developer? ")
+def main():
+    user_name = input("What is your name: ")
+    user_info = input("Where do you come from? : ")
+    user_favorite_color = input("What is your favorite color? : ")
+    user_favorite_food = input("What is your favorite food? : ")
+    user_work = input("What do you do for a living? :")
+    user_works = input("Are you a web developer? ")
     
-#     if user_works == "yes" :
-#         user_worked = input("What programming language are you learning ")
-#         user_working = input("What level of programming are you?, are you a beginner, professional... ")
+    if user_works == "yes" :
+        user_worked = input("What programming language are you learning ")
+        user_working = input("What level of programming are you?, are you a beginner, professional... ")
     
-#     print()
-    
-#     print("THOSE ARE ALL THE QUESTIONS, LOOK AT THE COMPREHENSION BELOW;")
-#     if user_works == "yes":
-#         print("My name is",user_name,".I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living. I am a",user_worked, "web developer. I am a",user_working,"in",user_worked)
-#         print()
-#         question = input("Is that the way you want your comprehension to be? (yes/no)? ")
-#         if question == "yes":
-#             question2 = input("Are you sure? (yes/no)")
-#             if question2 == "yes":
-#                 if user_works == "yes":
-#                     print()
-#                     print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living. I am a",user_worked, "web developer. I am a",user_working,"in",user_worked)
-#                 if user_works == "no":
-#                     print()
-#                     print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living." )
-#             else:           
-#                     user_name = input("What is your name? : ")
-#                     user_info = input("Where do you come from? : ")
-#                     user_favorite_color = input("What is your favorite color? : ")
-#                     user_favorite_food = input("What is your favorite food? : ")
-#                     user_work = input("What do you do for a living? :")
-#                     user_works = input("Are you a web developer? ")
-#                     if user_works == "yes" :
-#                         user_worked = input("What programming language are you learning ")
-#                         user_working = input("What level of programming are you?, are you a beginner, professional... ")
-#                         print()
-#                         print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked, "web developer. I am a",user_working,"in",user_worked )
-#                     else:
-#                         print()
-#                         print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
-#         else:
-#             user_name = input("What is your name: ")
-#             user_info = input("Where do you come from? : ")
-#             user_favorite_color = input("What is your favorite color? : ")
-#             user_favorite_food = input("What is your favorite food? : ")
-#             user_work = input("What do you do for a living? :")
-#             user_works = input("Are you a web developer? ")
-#             if user_works == "yes" :
-#                 user_worked = input("What programming language are you learning? ")
-#                 user_working = input("What level of programming are you?, are you a beginner, professional... ")
-#             if user_works == "yes":
-#                 print()
-#                 print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked,"web developer. I am a",user_working," in",user_worked )
-#             else:
-#                 print()
-#                 print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
-#     else:
-#         print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living.")
-#         print()
-#         question = input("Is that the way you want your comprehension to be? (yes/no)? ")
-#         if question == "yes":
-#             question2 = input("Are you sure? (yes/no)")
-#             if question2 == "yes":
-#                 if user_works == "yes":
-#                     print()
-#                     print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living. I am a",user_worked, "web developer. I am a",user_working,"in",user_worked)
-#                 if user_works == "no":
-#                     print()
-#                     print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living." )
-#             else:           
-#                     user_name = input("Enter your name: ")
-#                     user_info = input("Where do you come from: ")
-#                     user_favorite_color = input("What is your favorite color: ")
-#                     user_favorite_food = input("What is your favorite food: ")
-#                     user_work = input("What do you do for a living? :")
-#                     user_works = input("Are you a web developer? ")
-#                     if user_works == "yes" :
-#                         user_worked = input("What programming language are you learning ")
-#                         user_working = input("What level of programming are you?, are you a beginner, professional... ")
-#                         print()
-#                         print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked, "web developer. I am a",user_working,"in",user_worked )
-#                     else:
-#                         print()
-#                         print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
-#         else:
-
-#             user_name = input("Enter your name: ")
-#             user_info = input("Where do you come from: ")
-#             user_favorite_color = input("What is your favorite color: ")
-#             user_favorite_food = input("What is your favorite food: ")
-#             user_work = input("What do you do for a living? :")
-#             user_works = input("Are you a web developer? ")
-#             if user_works == "yes" :
-#                 user_worked = input("What programming language are you learning? ")
-#                 user_working = input("What level of programming are you?, are you a beginner, professional... ")
-#             if user_works == "yes":
-#                 print()
-#                 print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked,"web developer. I am a",user_working," in",user_worked )
-#             else:
-
-#                 print()
-#                 print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
-
-# main()
-
-while True:
-    print("WELCOME TO THE CALCULATOR APP, WHAT SERVICE DO YOU WANT TO ACHIEVE?")
-    print("1. ADDITION")
-    print("2. SUBTRACTION")
-    print("3. MULTIPLICATION") 
-    print("4. DIVISION")
-    print("5. QUIT")
     print()
-    options = input()
-    print()
-
-    if options.lower() == "1":
-        answer1 = input("Enter your first number: ")
-        answer2 = input("Enter your second number:")
-        print(answer1,"+",answer2," = ", str(int(answer1) + int(answer2)))
+    
+    print("THOSE ARE ALL THE QUESTIONS, LOOK AT THE COMPREHENSION BELOW;")
+    if user_works == "yes":
+        print("My name is",user_name,".I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living. I am a",user_worked, "web developer. I am a",user_working,"in",user_worked)
         print()
-    elif options.lower() == "2":
-        answer1 = input("Enter your first number: ")
-        answer2 = input("Enter your second number:")
-        print(answer1,"-",answer2," = ", str(int(answer1) - int(answer2)))
-        print()
-    elif options.lower() == "3":
-        answer1 = input("Enter your first number: ")
-        answer2 = input("Enter your second number:")
-        print(answer1,"*",answer2," = ", str(int(answer1) * int(answer2)))
-        print()
-    elif options.lower() == "4":
-        answer1 = input("Enter your first number: ")
-        answer2 = input("Enter your second number:")
-        print(answer1,"/",answer2," = ", str(int(answer1) / int(answer2)))
-        print()
-    elif options.lower() == "5":
-        print("Thanks for using the calculator-app")
-        print("Created by ImisiB")
-        quit()
+        question = input("Is that the way you want your comprehension to be? (yes/no)? ")
+        if question == "yes":
+            question2 = input("Are you sure? (yes/no)")
+            if question2 == "yes":
+                if user_works == "yes":
+                    print()
+                    print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living. I am a",user_worked, "web developer. I am a",user_working,"in",user_worked)
+                if user_works == "no":
+                    print()
+                    print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living." )
+            else:           
+                    user_name = input("What is your name? : ")
+                    user_info = input("Where do you come from? : ")
+                    user_favorite_color = input("What is your favorite color? : ")
+                    user_favorite_food = input("What is your favorite food? : ")
+                    user_work = input("What do you do for a living? :")
+                    user_works = input("Are you a web developer? ")
+                    if user_works == "yes" :
+                        user_worked = input("What programming language are you learning ")
+                        user_working = input("What level of programming are you?, are you a beginner, professional... ")
+                        print()
+                        print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked, "web developer. I am a",user_working,"in",user_worked )
+                    else:
+                        print()
+                        print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
+        else:
+            user_name = input("What is your name: ")
+            user_info = input("Where do you come from? : ")
+            user_favorite_color = input("What is your favorite color? : ")
+            user_favorite_food = input("What is your favorite food? : ")
+            user_work = input("What do you do for a living? :")
+            user_works = input("Are you a web developer? ")
+            if user_works == "yes" :
+                user_worked = input("What programming language are you learning? ")
+                user_working = input("What level of programming are you?, are you a beginner, professional... ")
+            if user_works == "yes":
+                print()
+                print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked,"web developer. I am a",user_working," in",user_worked )
+            else:
+                print()
+                print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
     else:
+        print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living.")
         print()
-        print("Wrong input!, you can type in only; '1', '2', '3','4' or '5'")
-        print()
+        question = input("Is that the way you want your comprehension to be? (yes/no)? ")
+        if question == "yes":
+            question2 = input("Are you sure? (yes/no)")
+            if question2 == "yes":
+                if user_works == "yes":
+                    print()
+                    print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living. I am a",user_worked, "web developer. I am a",user_working,"in",user_worked)
+                if user_works == "no":
+                    print()
+                    print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,".I do",user_work,"for a living." )
+            else:           
+                    user_name = input("Enter your name: ")
+                    user_info = input("Where do you come from: ")
+                    user_favorite_color = input("What is your favorite color: ")
+                    user_favorite_food = input("What is your favorite food: ")
+                    user_work = input("What do you do for a living? :")
+                    user_works = input("Are you a web developer? ")
+                    if user_works == "yes" :
+                        user_worked = input("What programming language are you learning ")
+                        user_working = input("What level of programming are you?, are you a beginner, professional... ")
+                        print()
+                        print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked, "web developer. I am a",user_working,"in",user_worked )
+                    else:
+                        print()
+                        print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
+        else:
+
+            user_name = input("Enter your name: ")
+            user_info = input("Where do you come from: ")
+            user_favorite_color = input("What is your favorite color: ")
+            user_favorite_food = input("What is your favorite food: ")
+            user_work = input("What do you do for a living? :")
+            user_works = input("Are you a web developer? ")
+            if user_works == "yes" :
+                user_worked = input("What programming language are you learning? ")
+                user_working = input("What level of programming are you?, are you a beginner, professional... ")
+            if user_works == "yes":
+                print()
+                print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living.I am a",user_worked,"web developer. I am a",user_working," in",user_worked )
+            else:
+
+                print()
+                print("My name is",user_name,"I come from",user_info,".My favorite color is",user_favorite_color,".My favorite food is",user_favorite_food,"I do",user_work,"for a living." )
+
+main()
+
